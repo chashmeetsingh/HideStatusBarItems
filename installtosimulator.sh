ee2f38021a320c2a7697bc2684e3609c084d4d18
Chashmeet Singh <chashmeetsingh@gmail.com>
2018-07-21 01:26:46 +0530

    Update installtosimulator.sh

--- installtosimulator.sh
+++ installtosimulator.sh
@@ -1,3 +1,3 @@
 cp .theos/obj/debug/HideStatusBarItems.dylib /opt/simject/
 cp .theos/obj/debug/HideStatusBarItems.bundle /opt/simject/
-ldid -S /opt/simject/VideoHUD.dylib
+ldid -S /opt/simject/HideStatusBarItems.dylib
